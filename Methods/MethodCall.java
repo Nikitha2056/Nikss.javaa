c5f92196f29e2d1a4a5a6a835f42afa518a4c450
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-12 10:40:54 +0530

    Create MethodCall.java

---
+++ Methods/MethodCall.java
@@ -0,0 +1,15 @@
+public class MethodCall {
+	
+	static void showCity() {
+		System.out.println("London");
+	}
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		showCity();
+		showCity(); 
+		}
+	}
+
+
