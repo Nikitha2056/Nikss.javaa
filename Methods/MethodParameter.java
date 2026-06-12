bf74e79a5ee791d3565e7f765395074078d41da0
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-12 13:49:59 +0530

    Add files via upload

---
+++ Methods/MethodParameter.java
@@ -0,0 +1,24 @@
+package LDay1;
+
+public class MethodParameter {
+
+	
+		
+		static void showCountry(String country) {
+			
+		System.out.println(country);
+		}
+
+		public static void main(String[] args) {
+			
+			
+			showCountry("INDIA");
+			showCountry("JAPAN");
+			showCountry("UK");
+			}
+
+		
+
+	}
+
+
