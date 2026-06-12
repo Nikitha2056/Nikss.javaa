57f93cd30151ccb89b5aebb1261453bd44a3181a
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-12 21:20:23 +0530

    Add files via upload

---
+++ Methods/Return.java
@@ -0,0 +1,18 @@
+package LDay1;
+
+public class Return {
+	
+	static int square(int x) {
+		
+		return(x * x);
+	}
+
+	public static void main(String[] args) {
+		
+		int result = square(4);
+		
+		System.out.println(result);
+		
+	}
+
+}
