709366dec34f515a7f8231e20838362edbc3e2b7
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-12 10:25:33 +0530

    Create Syntax.java

---
+++ Arrays/Syntax.java
@@ -0,0 +1,19 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+String[] colors = {"Red","Blue","Green","Yellow","Black"};
+
+    
+		System.out.println(colors[0]);
+		System.out.println(colors[3]);
+
+		
+		
+
+	}
+
+}
