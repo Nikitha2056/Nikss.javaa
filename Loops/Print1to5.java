22c9ff07fc725ff8e79e55e3959eb3014b052b39
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-12 10:21:34 +0530

    Update Print1to5.java

--- Loops/Print1to5.java
+++ Loops/Print1to5.java
@@ -1,1 +1,14 @@
+public class While1to5Print {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while(i <= 5) {
+			System.out.println(i);
+			i++;
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
 
