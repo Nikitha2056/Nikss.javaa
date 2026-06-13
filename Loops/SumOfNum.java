10f6cc7d18443783ed2b55f37baeb4db8f8493cf
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-13 20:26:46 +0530

    Add files via upload

---
+++ Loops/SumOfNum.java
@@ -0,0 +1,15 @@
+package LDay1;
+
+public class SumOfNum {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i<= 20; i++) {
+			sum = sum + i;
+		}
+		System.out.println(sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
