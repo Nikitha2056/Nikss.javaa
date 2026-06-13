80173e1058d1b88672da9fc82627c5fd2f4f27ed
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-13 12:28:51 +0530

    Add files via upload

---
+++ Loops/Print10to1.java
@@ -0,0 +1,13 @@
+package LDay1;
+
+public class Print10to1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 10; i >=1 ; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
