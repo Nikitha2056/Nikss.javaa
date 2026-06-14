96c6b0f76e292874736f27a4b86b3f693ca03c1e
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 16:22:15 +0530

    Add files via upload

---
+++ Strings/ReverseString.java
@@ -0,0 +1,14 @@
+package LDay1;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String movie = "Avatar";
+		for (int i = movie.length() - 1 ; i >= 0; i --) {
+			System.out.print(movie.charAt(i));
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
