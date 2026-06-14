1496ac7e34500a6cb7c6497c37731752532b73be
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 16:20:18 +0530

    Create StringTraversal.java

---
+++ Strings/StringTraversal.java
@@ -0,0 +1,23 @@
+package LDay1;
+
+public class StringTraversal {
+
+	public static void main(String[] args) {
+		String city = "London";
+		for (int i = 0; i < city.length(); i++) {
+			System.out.println(city.charAt(i));
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+L
+o
+n
+d
+o
+n
