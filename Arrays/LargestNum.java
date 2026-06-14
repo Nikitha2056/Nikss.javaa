68ce9af684230086492dc663f25aed16c7ae0d96
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 17:34:07 +0530

    Add files via upload

---
+++ Arrays/LargestNum.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class LargestNum {
+
+	public static void main(String[] args) {
+		int nums[] = {10,20,30,40,50,90,80};
+		int max = nums[0];
+		for(int i = 0; i < nums.length; i++) {
+			if (nums[i] > max) {
+				max = nums[i];
+			}
+		}
+		System.out.println(max);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
