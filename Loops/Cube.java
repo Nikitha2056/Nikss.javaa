c867232d59ee65848d8021a7de106a32aca23c49
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 08:31:27 +0530

    Add files via upload

---
+++ Loops/Cube.java
@@ -0,0 +1,17 @@
+package LDay1;
+
+public class Cube {
+	
+	static int cube(int x) {
+		return x * x * x;
+	}
+
+	public static void main(String[] args) {
+		int result = cube(9);
+		System.out.println(result);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
