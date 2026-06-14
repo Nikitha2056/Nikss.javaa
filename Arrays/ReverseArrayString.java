55edfa4f7ec39ff7dd6c006e0b24027501a7236e
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 19:56:44 +0530

    Add files via upload

---
+++ Arrays/ReverseArrayString.java
@@ -0,0 +1,15 @@
+package day4;
+
+public class ReverseArrayString {
+
+	public static void main(String[] args) {
+		int nums[] = {10,20,30,40,50};
+		for(int i = nums.length - 1; i >= 0; i--) {
+			System.out.println(nums[i]);
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
