fa088f1cf34fea8de480aae9d7a2366c1962df10
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 17:32:06 +0530

    Add files via upload

---
+++ Arrays/ArrayTraversal.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class ArrayTraversal {
+
+	public static void main(String[] args) {
+		int marks[] = {95,88,76,99,67};
+		for (int i = 0; i < marks.length;i++) {
+			System.out.println(marks[i]);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
