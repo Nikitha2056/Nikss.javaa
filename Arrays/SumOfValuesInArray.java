8dfdd8dd90966aa69234584c22fbfd96ec936642
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 17:43:26 +0530

    Add files via upload

---
+++ Arrays/SumOfValuesInArray.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class SumOfValuesInArray {
+
+	public static void main(String[] args) {
+		int nums[] = {20,30,40,50};
+		int sum = 0;
+		for(int i = 0; i < nums.length;i++) {
+			sum = sum + nums[i];
+		}
+		System.out.println(sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
