22be95a71600662cbbcbf6a30672a4915a4d96df
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 18:18:26 +0530

    Add files via upload

---
+++ Patterns/TrianglePattern.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class TrianglePattern {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 3; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
