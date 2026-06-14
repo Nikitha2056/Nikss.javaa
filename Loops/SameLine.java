0c9d5a2b5a4b6e54cd171929251bf56c99fc2bf7
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 08:32:37 +0530

    Add files via upload

---
+++ Loops/SameLine.java
@@ -0,0 +1,13 @@
+package LDay1;
+
+public class SameLine {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.print(i + " ");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
