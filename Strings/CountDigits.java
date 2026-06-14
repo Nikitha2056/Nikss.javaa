2bae02c8a05a00c37aa58be8a3c2bba241b76344
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-14 17:35:21 +0530

    Add files via upload

---
+++ Strings/CountDigits.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		String text = "abc123";
+		int count = 0;
+		for (int i = 0; i < text.length(); i++) {
+			char ch = text.charAt(i);
+			if (ch >= '0' && ch <= '9') {
+				count++;
+			}
+		}
+		System.out.println(count);
+		// TODO Auto-generated method stub
+
+	}
+
+}
