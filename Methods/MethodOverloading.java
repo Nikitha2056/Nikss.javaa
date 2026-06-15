c2af9662f094bc6a5961809ac987d92f4b0e3e02
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-15 13:23:02 +0530

    Add files via upload

---
+++ Methods/MethodOverloading.java
@@ -0,0 +1,18 @@
+
+public class MethodOverloading {
+	static void show(int x) {
+		System.out.println(x);
+	}
+	static void show(String name) {
+		System.out.println( name);
+	}
+
+	public static void main(String[] args) {
+		show(20);
+		show("Niks");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
