a64f3c1cd8e04120b1bbcaaa4cac67f607b01875
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-16 14:20:28 +0530

    Add files via upload

---
+++ Strings/CountNoOfWords.java
@@ -0,0 +1,22 @@
+package day5;
+
+public class CountNoOfWords {
+
+	public static void main(String[] args) {
+		String word = "Kothamekala nikitha Reddy";
+		
+		int words = 1;
+		for(int i = 0; i < word.length();i++) {
+			
+			if(word.charAt(i) == ' ') {
+				words++;
+			}
+		}
+		System.out.println("Words = " + words );
+	
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
