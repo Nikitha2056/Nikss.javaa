87b6ef2085602a8bbfd68fe0e3f615618a7dd919
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-16 20:48:06 +0530

    Add files via upload

---
+++ Strings/RemoveSpaces.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class RemoveSpaces {
+
+	public static void main(String[] args) {
+		String word = "Java DSA SQL";
+		
+		for(int i = 0; i < word.length();i++) {
+			char ch = word.charAt(i);
+			if(ch != ' ') {
+				System.out.print(ch);
+			}
+		}
+		System.out.println();
+	
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
