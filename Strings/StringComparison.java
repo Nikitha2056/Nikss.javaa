4e7e0d88cf74f6f1dd86535f26fe21e48f9b2b73
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-16 21:13:51 +0530

    Add files via upload

---
+++ Strings/StringComparison.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class StringComparison {
+
+	public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "java";
+        if(s1.equals(s2)) {
+        	System.out.println("same");
+        } else {
+        	System.out.println("different");
+        }
+		
+		
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
