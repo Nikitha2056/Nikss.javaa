15a13804233acd42dde6aa07095c2f54e56b454c
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-16 14:05:04 +0530

    Add files via upload

---
+++ Strings/CountLowerCaseLetter.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class CountLowerCaseLetter {
+
+	public static void main(String[] args) {
+		String word = "JaVa";
+		int count = 0;
+		for(int i = 0; i < word.length();i++) {
+			char ch = word.charAt(i);
+			if(ch >= 'a' && ch <= 'z') {
+				count++;
+			}
+		}
+		System.out.println("Count = " + count );
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
