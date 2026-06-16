875bd356e2e07b27e2d05cb36b48c05d04976f0c
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-16 14:04:40 +0530

    Add files via upload

---
+++ Strings/CountUpperCaseLetter.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class CountUpperCaseLetter {
+
+	public static void main(String[] args) {
+		String word = "JaVa";
+		int count = 0;
+		for(int i = 0; i < word.length();i++) {
+			char ch = word.charAt(i);
+			if(ch >= 'A' && ch <= 'Z') {
+				count++;
+			}
+		}
+		System.out.println("Count = " + count );
+		
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
