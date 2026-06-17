86a2417777ceb84972b8e6c73ad9cb1ca3427546
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-17 13:45:06 +0530

    Add files via upload

---
+++ Strings/SplitWords.java
@@ -0,0 +1,17 @@
+package day7;
+
+public class SplitWords {
+
+	public static void main(String[] args) {
+		String sentence = "JAVA DSA";
+		String[] words = sentence.split(" ");
+		
+		System.out.print(words[0] + " ");
+		System.out.print(words[1]);
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
