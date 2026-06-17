c68a2e5c91635bc1c3109c1ad29f1fac11d6400a
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-17 13:43:39 +0530

    Add files via upload

---
+++ Reverse/ReverseEntireSentence.java
@@ -0,0 +1,14 @@
+package day7;
+
+public class ReverseEntireSentence {
+
+	public static void main(String[] args) {
+		String sentence = "Kothamekala Nikitha Reddy";
+		for(int i = sentence.length()-1; i >= 0; i--) {
+			System.out.print(sentence.charAt(i));
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
