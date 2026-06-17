28d3032e61130a648fbe8de34831a39ba3d0102c
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-17 16:08:59 +0530

    Add files via upload

---
+++ Strings/DuplicateCharacterOnlyCharacterPrint.java
@@ -0,0 +1,27 @@
+package day7;
+
+public class DuplicateCharacterOnlyCharacterPrint {
+
+	public static void main(String[] args) {
+        String word = "banana";
+		
+		for(int i = 0; i < word.length(); i++) {
+			int count = 0;
+			
+			for(int j = 0; j < word.length(); j++) {
+				
+				if(word.charAt(i) == word.charAt(j)) {
+					count++;
+				}
+			}
+			
+			if(count > 1) {
+				System.out.println(word.charAt(i));
+			}
+			
+		}
+		
+
+	}
+
+}
