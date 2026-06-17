4030fb12835a0f71f23b5e9aeb848a62d3c524f2
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-17 13:42:13 +0530

    Add files via upload

---
+++ Reverse/ReverseEachWord.java
@@ -0,0 +1,26 @@
+package day7;
+
+public class ReverseEachWord {
+
+	public static void main(String[] args) {
+		String sentence = "JAVA DSA SQL";
+		String[] words = sentence.split(" ");
+		
+		for(int i = words[0].length()-1; i >= 0; i--) {
+			System.out.print(words[0].charAt(i));
+		}
+		System.out.print(" ");
+	
+		for(int i = words[1].length()-1; i >= 0; i--) {
+			System.out.print(words[1].charAt(i));
+		}
+		System.out.print(" ");
+		
+		for(int i = words[2].length()-1; i >= 0; i--) {
+			System.out.print(words[2].charAt(i));
+		}
+		
+		System.out.print(" ");
+	}
+
+}
