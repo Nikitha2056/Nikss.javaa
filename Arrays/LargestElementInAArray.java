3902eb7c83647296e7bb9e44b218873bdfee7df5
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-19 16:40:47 +0530

    Add files via upload

---
+++ Arrays/LargestElementInAArray.java
@@ -0,0 +1,19 @@
+package day8;
+
+public class LargestElementInAArray {
+
+	public static void main(String[] args) {
+		int arr[] = {25,10,80,45,60};
+		int largest = arr[0];
+		for(int i = 0; i < arr.length;i++) {
+			if(arr[i] > largest) {
+				largest = arr[i];
+			}
+			
+		}
+		System.out.println(largest);
+		// TODO Auto-generated method stub
+
+	}
+
+}
