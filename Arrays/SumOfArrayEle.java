e1787db9555dd3558aaa02ffb8875cacd6c765dd
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-19 16:42:31 +0530

    Add files via upload

---
+++ Arrays/SumOfArrayEle.java
@@ -0,0 +1,16 @@
+package day8;
+
+public class SumOfArrayEle {
+
+	public static void main(String[] args) {
+		int arr[] = {10,20,30,40,50};
+		int sum = 0; 
+		for(int i = 0; i < arr.length;i++) {
+			sum = sum + arr[i];
+		}
+		System.out.println(sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
