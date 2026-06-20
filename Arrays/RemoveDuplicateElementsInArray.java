c24b81f564ef17eefa2b96426eb8fb3610c52310
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-20 19:00:13 +0530

    Add files via upload

---
+++ Arrays/RemoveDuplicateElementsInArray.java
@@ -0,0 +1,22 @@
+package day10;
+
+public class RemoveDuplicateElementsInArray {
+
+	public static void main(String[] args) {
+		int arr[] = {5,10,5,15,10,20};
+		for(int i = 0; i < arr.length;i++) {
+			boolean alreadySeen = false;
+			for(int j = 0; j < i;j++) {
+				if(arr[i] == arr[j]) {
+					alreadySeen = true;
+				}
+			}
+			if(!alreadySeen) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
