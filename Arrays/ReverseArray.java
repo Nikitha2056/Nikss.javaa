3f6e6c6d729c8c6769f13b84243715b4b258bcd5
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-20 10:58:07 +0530

    Add files via upload

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,15 @@
+package day9;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		int arr[] = {5,10,15,20};
+		for(int i = arr.length-1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
