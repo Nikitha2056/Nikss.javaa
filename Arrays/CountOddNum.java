d6e8838bf7890bebb413155b139509a21a4462e9
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-20 10:57:10 +0530

    Add files via upload

---
+++ Arrays/CountOddNum.java
@@ -0,0 +1,21 @@
+package day9;
+
+public class CountOddNum {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,67,890};
+		int count = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 != 0) {
+				count++;
+
+			}
+			
+		}
+		System.out.println("Count = " + count);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
