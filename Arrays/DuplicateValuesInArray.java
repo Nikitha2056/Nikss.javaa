f33a84bf5be3aef5366b256489e80fa69ff8e599
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-20 18:52:44 +0530

    Add files via upload

---
+++ Arrays/DuplicateValuesInArray.java
@@ -0,0 +1,19 @@
+package day10;
+
+public class DuplicateValuesInArray {
+
+	public static void main(String[] args) {
+		int arr[] = {5,10,15,10,20,5};
+		
+		for(int i = 0; i < arr.length;i++) {
+			// Current element tarvata unna elements tho matrame compare cheyyi
+			for(int j = i + 1 ; j < arr.length;j++) {
+				if(arr[i] == arr[j]) {
+					System.out.println(arr[i]);
+				}
+			}
+		}		// TODO Auto-generated method stub
+
+	}
+
+}
