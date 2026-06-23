40d21630e2d0f7852f3d3871363c9ddec5e692a7
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-23 20:01:27 +0530

    Add files via upload

---
+++ Sorting Techniques/BubbleSort.java
@@ -0,0 +1,31 @@
+package day13;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {50,20,40,10};
+
+        for(int i = 0; i < arr.length - 1; i++) {
+
+            for(int j = 0; j < arr.length - 1 - i; j++) {
+
+                if(arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+
+            System.out.print(arr[i] + " ");
+        }
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
