88f39e057f8ad4c9a0763f6a31289ad8f6df4751
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-25 21:32:53 +0530

    Add files via upload

---
+++ SearchingTechniques/MissingNumber.java
@@ -0,0 +1,19 @@
+package day14;
+
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3,5};
+		int n = 5;
+		int expectedSum = n * (n + 1) / 2;
+		int actualSum = 0;
+		for(int i = 0; i < arr.length; i++) {
+			actualSum = actualSum + arr[i];
+		}
+		
+		int missing = expectedSum - actualSum;
+		System.out.print("Missing Number = " + missing);
+	}
+
+}
