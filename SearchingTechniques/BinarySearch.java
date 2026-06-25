c23748ed6b53b0a6778de49a4543fbf707134b67
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-25 21:31:09 +0530

    Add files via upload

---
+++ SearchingTechniques/BinarySearch.java
@@ -0,0 +1,27 @@
+package day14;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+	
+		int arr[] = {10,20,30,40,50,60,70};
+		int target = 60;
+		
+		int low = 0;
+		int high = arr.length - 1;
+		
+		while(low <= high) {
+			int mid = (low + high) / 2;
+			if(arr[mid] == target) {
+				System.out.println("Found at index = " + mid);
+				break;
+			} else if (target > arr[mid]) {
+				low =mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+
+	}
+
+}
