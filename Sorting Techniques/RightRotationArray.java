3b49a388e6cb2612671e2cbf3c519255fcaecc4a
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-27 20:08:17 +0530

    Add files via upload

---
+++ Sorting Techniques/RightRotationArray.java
@@ -0,0 +1,20 @@
+package day14;
+
+public class RightRotationArray {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {10,20,30,40,50};
+		int temp = arr[arr.length - 1];
+		for(int i = arr.length - 1; i > 0; i--) {
+			arr[i] = arr[i-1];
+		}
+		arr[0] = temp;
+		System.out.print("Right Rotated Array = ");
+		for(int i = 0; i < arr.length;i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+	}
+
+}
