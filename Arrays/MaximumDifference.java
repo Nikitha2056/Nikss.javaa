815813adba13e3911db4dd364216db50f7284f59
Nikitha2056 <nikithasri9631@gmail.com>
2026-07-03 09:21:17 +0530

    Add files via upload

---
+++ Arrays/MaximumDifference.java
@@ -0,0 +1,23 @@
+package day18;
+
+public class MaximumDifference {
+
+	public static void main(String[] args) {
+		int arr[] = {15,40,10,60,25};
+		int max = arr[0];
+		int min = arr[0];
+		for(int i = 0; i < arr.length;i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+			if(arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		int difference = max - min;
+		System.out.println("Difference = " + difference);
+		// TODO Auto-generated method stub
+
+	}
+
+}
