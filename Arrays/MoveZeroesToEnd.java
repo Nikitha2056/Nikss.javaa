fa3dc460cfe93c307b382ed8c739adc5a04aa813
Nikitha2056 <nikithasri9631@gmail.com>
2026-07-03 09:22:17 +0530

    Add files via upload

---
+++ Arrays/MoveZeroesToEnd.java
@@ -0,0 +1,28 @@
+package day18;
+
+public class MoveZeroesToEnd {
+
+	public static void main(String[] args) {
+		
+		// Moving all the non-zero elements to first
+		int arr[] = {5,0,8,0,12,15};
+		int index = 0;
+		for(int i =0; i< arr.length;i++) {
+			if(arr[i] != 0) {
+				arr[index] = arr[i];
+				index++;
+			}
+		}
+	// Filling the remaining positions with the zeroes
+		while(index < arr.length) {
+			arr[index] = 0;
+			index++;
+		}
+		System.out.print("After Moving Zeroes = ");
+		for(int i = 0; i < arr.length;i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+}
