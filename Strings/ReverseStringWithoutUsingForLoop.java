3c33c712d4b03e2433258b0ffd9802e4fdd3db26
Nikitha2056 <nikithasri9631@gmail.com>
2026-07-03 20:39:21 +0530

    Add files via upload

---
+++ Strings/ReverseStringWithoutUsingForLoop.java
@@ -0,0 +1,14 @@
+package day19;
+
+public class ReverseStringWithoutUsingForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		String str = "JAVA";
+		String rev = new StringBuilder(str).reverse().toString();
+		System.out.print(rev);
+
+
+	}
+
+}
