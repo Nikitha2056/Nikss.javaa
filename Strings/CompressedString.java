1b636ac6bf35ce3123c2fb22b22df111e6469d1d
Nikitha2056 <nikithasri9631@gmail.com>
2026-07-04 16:21:26 +0530

    Add files via upload

---
+++ Strings/CompressedString.java
@@ -0,0 +1,27 @@
+package day20;
+
+public class CompressedString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "aabbbcccc";
+		String compressed = "";
+		int count = 1;
+		for(int i = 0; i < str.length()-1;i++) {
+			if(str.charAt(i) == str.charAt(i+1)) {
+				count++;
+			}
+			else {
+				compressed += str.charAt(i);
+				compressed += count;
+				count = 1;
+			}
+		}
+		
+		compressed += str.charAt(str.length()-1);
+		compressed += count;
+		System.out.println(compressed);
+
+	}
+
+}
