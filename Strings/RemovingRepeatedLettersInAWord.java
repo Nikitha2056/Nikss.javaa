519c7c0a6d2030d9acb1d5fdccf1fdc1618f2cdb
Nikitha2056 <nikithasri9631@gmail.com>
2026-07-11 18:46:06 +0530

    Add files via upload

---
+++ Strings/RemovingRepeatedLettersInAWord.java
@@ -0,0 +1,23 @@
+package day21;
+
+public class RemovingRepeatedLettersInAWord {
+
+	public static void main(String[] args) {
+		String word = "programming";
+		String result = "";
+		for(int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			// this line is been used to skip the letter if it is been repeated
+			if(result.indexOf(ch) == -1) {
+				result += ch;
+			}
+			
+		}
+		System.out.print(result);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
