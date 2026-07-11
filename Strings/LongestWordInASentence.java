5017bce706e5ad0c0c320e7824fccd5b7b13a393
Nikitha2056 <nikithasri9631@gmail.com>
2026-07-11 18:44:51 +0530

    Add files via upload

---
+++ Strings/LongestWordInASentence.java
@@ -0,0 +1,22 @@
+package day21;
+
+public class LongestWordInASentence {
+
+	public static void main(String[] args) {
+		
+		String sentence = "I love Java Programming";
+		String words[] = sentence.split(" ");
+		String longest = "";
+		
+		for(int i = 0; i < words.length;i++) {
+			
+			if(words[i].length() > longest.length()) {
+				longest = words[i];
+			}
+		}
+		System.out.print("Longest Word = " + longest);
+		// TODO Auto-generated method stub
+
+	}
+
+}
