31d9d3161f3b5a6d932986c22585910a0feb480e
Nikitha2056 <nikithasri9631@gmail.com>
2026-08-18 19:36:00 +0530

    Add files via upload

---
+++ Sorting Techniques/QuickSort.java
@@ -0,0 +1,40 @@
+package day30;
+
+public class QuickSort {
+	
+	public static void quickSort(int arr[] , int low, int high) {
+		if(low < high) {
+			int pivotIndex = partition(arr, low, high);
+			quickSort(arr, low, pivotIndex - 1);
+			quickSort(arr, pivotIndex + 1, high);
+		}
+	}
+	public static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1;
+		for(int j = low; j < high; j++) {
+			if(arr[j] < pivot) {
+				i++;
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+		
+		return i + 1;
+	}
+public static void main(String[] args) {
+		int arr[] = {10, 40, 30, 20};
+		quickSort(arr, 0, arr.length - 1);
+		System.out.println("Sorted Array : ");
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		// TODO Auto-generated method stub
+		
+	}
+
+}
